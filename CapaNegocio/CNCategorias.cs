0df1cc6d9f8117b8f6717801eda57e4e72a9a0ba
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-09 21:30:45 -0700

    Agregue los metodos Listar, editar, Guardar, Eliminar y BuscarNombre a la clase CNCategorias de la capa negocio

--- CapaNegocio/CNCategorias.cs
+++ CapaNegocio/CNCategorias.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
 
 namespace CapaNegocio
 {
-    internal class CNCategorias
+    public class CNCategorias
     {
+        public static DataTable Listar()
+        { 
+            CDCategorias Datos = new CDCategorias();
+            return Datos.Listar();
+        }
+
+        public static string Guardar(String nombre)
+        { 
+            CDCategorias Datos = new CDCategorias();
+            Datos.nombre = nombre;
+            return Datos.Guardar(Datos);
+        }
+
+        public static string Editar(int id_categoria, string nombre)
+        { 
+            CDCategorias Datos = new CDCategorias();
+            Datos.id_categoria = id_categoria;
+            Datos.nombre = nombre;
+            return Datos.Editar(Datos);
+        }
+
+        public static string Eliminar(int id_categoria)
+        { 
+            CDCategorias Datos = new CDCategorias();
+            Datos.id_categoria = id_categoria;
+            return Datos.Eliminar(Datos);
+        }
+
+        public static DataTable BuscarNombre(string textoBuscar)
+        { 
+            CDCategorias Datos = new CDCategorias();
+            Datos.Buscar = textoBuscar;
+            return Datos.BuscarNombre(Datos);
+        }
     }
 }
