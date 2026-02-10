8133f08d2e9337935610a90ce836dad092dbe28c
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-09 22:09:26 -0700

    cree el formulario para listar categorias y le añadi metodos para que funcione

---
+++ CapaPresentacion/FrmListadoCategoriascs.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmListadoCategoriascs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.dlistado = new System.Windows.Forms.DataGridView();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dlistado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categorias";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblnombre);
+            this.panel1.Controls.Add(this.btnsalir);
+            this.panel1.Controls.Add(this.btnbuscar);
+            this.panel1.Controls.Add(this.txtbuscar);
+            this.panel1.Location = new System.Drawing.Point(17, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(415, 73);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(3, 16);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(56, 16);
+            this.lblnombre.TabIndex = 3;
+            this.lblnombre.Text = "Nombre";
+            this.lblnombre.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(320, 35);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 2;
+            this.btnsalir.Text = "&Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(239, 34);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 1;
+            this.btnbuscar.Text = "&Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Location = new System.Drawing.Point(6, 35);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(209, 22);
+            this.txtbuscar.TabIndex = 0;
+            // 
+            // dlistado
+            // 
+            this.dlistado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dlistado.Location = new System.Drawing.Point(17, 116);
+            this.dlistado.Name = "dlistado";
+            this.dlistado.RowHeadersWidth = 51;
+            this.dlistado.RowTemplate.Height = 24;
+            this.dlistado.Size = new System.Drawing.Size(415, 150);
+            this.dlistado.TabIndex = 2;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(175, 292);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnnuevo.TabIndex = 3;
+            this.btnnuevo.Text = "&Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            // 
+            // btneditar
+            // 
+            this.btneditar.Location = new System.Drawing.Point(256, 292);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(75, 23);
+            this.btneditar.TabIndex = 4;
+            this.btneditar.Text = "&Editar";
+            this.btneditar.UseVisualStyleBackColor = true;
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(337, 292);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 5;
+            this.btneliminar.Text = "&Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            // 
+            // FrmListadoCategoriascs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 340);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.dlistado);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmListadoCategoriascs";
+            this.Text = "FrmListadoCategoriascs";
+            this.Load += new System.EventHandler(this.FrmListadoCategoriascs_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dlistado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.DataGridView dlistado;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btneliminar;
+    }
+}
---
+++ CapaPresentacion/FrmListadoCategoriascs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class FrmListadoCategoriascs : Form
+    {
+        public FrmListadoCategoriascs()
+        {
+            InitializeComponent();
+        }
+       
+       //esta pinche etiqueta no la pude quitar xd luego veo como la quito
+       private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void FrmListadoCategoriascs_Load(object sender, EventArgs e)
+        {
+            this.Top = 0;
+            this.Left = 0;
+        }
+
+        public void Mostrar()
+        { 
+            this.dlistado.DataSource = CNCategorias.Listar();
+        }
+
+        public void BuscarNombre()
+        { 
+            this.dlistado.DataSource = CNCategorias.BuscarNombre(this.txtbuscar.Text);
+        }
+
+        private void FrmListadoCategorias_Load(object sender, EventArgs e)
+        {
+            this.Top = 0;
+            this.Left = 0;
+            Mostrar();
+        }
+    }
+}
