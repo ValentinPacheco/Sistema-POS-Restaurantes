434427a350937d5b26965ec36dda6998637a1709
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-09 20:46:00 -0700

    Definir las propiedades de la clase CDCategorias

---
+++ CapaDatos/CDCategorias.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class CDCategorias
+    {
+        public int id_categoria { get; set; }
+        public string nombre { get; set; }
+
+        public string Buscar { get; set; }
+    }
+}
