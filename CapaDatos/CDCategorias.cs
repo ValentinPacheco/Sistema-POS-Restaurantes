fa053d70bd0a89489273536875707d3db41407f8
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-09 21:16:32 -0700

    Agregué los metodos Listar, Editar, Guardar, Eliminar y BuscarNombre a  CDCategorias

--- CapaDatos/CDCategorias.cs
+++ CapaDatos/CDCategorias.cs
@@ -14,5 +14,152 @@ namespace CapaDatos
         public string nombre { get; set; }
 
         public string Buscar { get; set; }
+
+        public DataTable Listar()
+        {
+            DataTable resul = new DataTable("categorias");
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                conexion.ConnectionString = Conexion.Conn;
+                SqlCommand cmd = new SqlCommand("splistar_categorias", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(cmd);
+                SqlDat.Fill(resul);
+            }
+            catch (Exception ex)
+            {
+                resul = null;
+                throw ex;
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+
+            return resul;
+        }
+
+        public string Guardar(CDCategorias cli)
+        {
+            string resul = "";
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                conexion.ConnectionString = Conexion.Conn;
+                conexion.Open();
+                SqlCommand Cmd = new SqlCommand("spguardar_categorias", conexion);
+                Cmd.CommandType = CommandType.StoredProcedure;
+
+                Cmd.Parameters.AddWithValue("@id_categoria", SqlDbType.Int).Direction = ParameterDirection.Output;
+                Cmd.Parameters.AddWithValue("@nombre", cli.nombre);
+
+                resul = Cmd.ExecuteNonQuery() == 1 ? "OK" : "No se pudo insertar el registro";
+            }
+            catch (Exception ex)
+            {
+                resul = ex.Message;
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+            return resul;
+        }
+
+        public string Editar(CDCategorias cli)
+        {
+            string resul = "";
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                conexion.ConnectionString = Conexion.Conn;
+                conexion.Open();
+                SqlCommand Cmd = new SqlCommand("speditar_categorias", conexion);
+                Cmd.CommandType = CommandType.StoredProcedure;
+
+                Cmd.Parameters.AddWithValue("@id_categoria", cli.id_categoria);
+                Cmd.Parameters.AddWithValue("@nombre", cli.nombre);
+
+                resul = Cmd.ExecuteNonQuery() == 1 ? "OK" : "No se pudo actualizar el registro";
+            }
+            catch (Exception ex)
+            {
+                resul = ex.Message;
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+            return resul;
+        }
+
+        public string Eliminar(CDCategorias cli)
+        {
+            string resul = "";
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                conexion.ConnectionString = Conexion.Conn;
+                conexion.Open();
+                SqlCommand Cmd = new SqlCommand("speliminar_categorias", conexion);
+                Cmd.CommandType = CommandType.StoredProcedure;
+
+                Cmd.Parameters.AddWithValue("@id_categoria", cli.id_categoria);
+
+                resul = Cmd.ExecuteNonQuery() == 1 ? "OK" : "No se pudo eliminar el registro";
+            }
+            catch (Exception ex)
+            {
+                resul = ex.Message;
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+            return resul;
+        }
+
+        public DataTable BuscarNombre(CDCategorias cli)
+        { 
+            DataTable resul = new DataTable("categorias");
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                conexion.ConnectionString = Conexion.Conn;
+                SqlCommand cmd = new SqlCommand("spbuscar_categorias_nombre", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombre", cli.Buscar);
+                SqlDataAdapter SqlDat = new SqlDataAdapter(cmd);
+                SqlDat.Fill(resul);
+            }
+            catch (Exception ex)
+            {
+                resul = null;
+                throw ex;
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+            return resul;
+        }
     }
+
 }
