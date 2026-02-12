ad10724b0f0485cf07d55371845c5bc98b0b166f
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-12 09:17:48 -0700

    Estructura inicial de CapaEntidades y CapaNegocio
    Se agregan los proyectos CapaEntidades y CapaNegocio, incluyendo la clase de entidad CEUsuario y la clase de negocio CNUsuario. Se eliminan archivos y referencias innecesarias.

--- ENTIDADES/Usuarios.cs
+++ ENTIDADES/Usuarios.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaNegocio
+namespace ENTIDADES
 {
-    public class Class1
+    internal class Usuarios
     {
     }
 }
