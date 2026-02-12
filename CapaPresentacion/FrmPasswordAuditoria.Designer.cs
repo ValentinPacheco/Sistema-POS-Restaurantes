3969566752873260d54fbc68851a29a3ad9e6b9c
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-12 10:35:37 -0700

    Añade formulario de contraseña para auditoría
    Se incorpora el formulario `FrmPasswordAuditoria` que solicita una contraseña para acceder a la auditoría. Se agregan los archivos de código, diseño y recursos, y se actualiza el archivo del proyecto para incluirlos.

---
+++ CapaPresentacion/FrmPasswordAuditoria.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmPasswordAuditoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.lblcontraseña = new System.Windows.Forms.Label();
+            this.btnaceptar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(57, 84);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(217, 22);
+            this.txtContraseña.TabIndex = 0;
+            this.txtContraseña.UseSystemPasswordChar = true;
+            // 
+            // lblcontraseña
+            // 
+            this.lblcontraseña.AutoSize = true;
+            this.lblcontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontraseña.Location = new System.Drawing.Point(32, 40);
+            this.lblcontraseña.Name = "lblcontraseña";
+            this.lblcontraseña.Size = new System.Drawing.Size(262, 25);
+            this.lblcontraseña.TabIndex = 1;
+            this.lblcontraseña.Text = "INGRESE CONTRASEÑA";
+            // 
+            // btnaceptar
+            // 
+            this.btnaceptar.Location = new System.Drawing.Point(57, 165);
+            this.btnaceptar.Name = "btnaceptar";
+            this.btnaceptar.Size = new System.Drawing.Size(88, 23);
+            this.btnaceptar.TabIndex = 2;
+            this.btnaceptar.Text = "ACEPTAR";
+            this.btnaceptar.UseVisualStyleBackColor = true;
+            this.btnaceptar.Click += new System.EventHandler(this.btnaceptar_Click);
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.Location = new System.Drawing.Point(186, 165);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(88, 23);
+            this.btncancelar.TabIndex = 3;
+            this.btncancelar.Text = "CANCELAR";
+            this.btncancelar.UseVisualStyleBackColor = true;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // FrmPasswordAuditoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 242);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btnaceptar);
+            this.Controls.Add(this.lblcontraseña);
+            this.Controls.Add(this.txtContraseña);
+            this.Name = "FrmPasswordAuditoria";
+            this.Text = "FrmPasswordAuditoria";
+            this.Load += new System.EventHandler(this.FrmPasswordAuditoria_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label lblcontraseña;
+        private System.Windows.Forms.Button btnaceptar;
+        private System.Windows.Forms.Button btncancelar;
+    }
+}
---
+++ CapaPresentacion/FrmPasswordAuditoria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FrmPasswordAuditoria : Form
+    {
+        public bool AccesoPermitido { get; private set; } = false;
+
+        public FrmPasswordAuditoria()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPasswordAuditoria_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnaceptar_Click(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "1234")
+            {
+                AccesoPermitido = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Contraseña incorrecta");
+            }
+        }
+
+        private void btncancelar_Click(object sender, EventArgs e)
+        {
+            AccesoPermitido = false;
+            this.Close();
+        }
+    }
+}
