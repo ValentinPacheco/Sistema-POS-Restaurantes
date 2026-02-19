60d447ab6a63caa6feb31d75edac575845a89788
ValentinPacheco <valepachecoolivas@gmail.com>
2026-02-19 07:22:18 -0700

    trate de hacer un login pero no jaló, lo deja´re para mas tarde y hare otras cosas xd

---
+++ CapaDatos/CDUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaDatos;
+
+namespace CapaDatos
+{
+    public class CDUsuario
+    {
+            private Conexion Conn= new Conexion();
+
+    public DataTable Login(string username)
+    {
+        SqlCommand cmd = new SqlCommand(
+            "SELECT u.id_usuario, u.nombre, u.username, u.password_hash, r.nombre AS rol " +
+            "FROM usuarios u " +
+            "INNER JOIN roles r ON u.id_rol = r.id_rol " +
+            "WHERE u.username = @username AND u.activo = 1",
+            Conn.AbrirConexion());
+
+        cmd.Parameters.AddWithValue("@username", username);
+
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+
+        Conn.CerrarConexion();
+        return dt;
+    }
+
+        public DataTable ObtenerUsuario(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CapaNegocio/CNUsuario.cs
@@ -0,0 +1,53 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace CapaNegocio
+{
+    public class CNUsuario
+    {
+        private CDUsuario datos = new CDUsuario();
+
+        public bool Login(string username, string password)
+        {
+            DataTable dt = datos.ObtenerUsuario(username);
+
+            if (dt.Rows.Count == 0)
+                return false;
+
+            string hashBD = dt.Rows[0]["password_hash"].ToString();
+            string hashIngresado = ObtenerSHA256(password);
+
+            if (hashBD == hashIngresado)
+            {
+                CNSesion.IdUsuario = Convert.ToInt32(dt.Rows[0]["id_usuario"]);
+                CNSesion.Nombre = dt.Rows[0]["nombre"].ToString();
+                CNSesion.Rol = dt.Rows[0]["rol"].ToString();
+                return true;
+            }
+
+            return false;
+        }
+
+        private string ObtenerSHA256(string texto)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(texto));
+                StringBuilder sb = new StringBuilder();
+
+                foreach (byte b in bytes)
+                    sb.Append(b.ToString("x2"));
+
+                return sb.ToString();
+            }
+        }
+    }
+}
