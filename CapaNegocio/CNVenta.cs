3e4fc6805a0add22f15ce5bb6bf91460afad1128
ValentinPacheco <valepachecoolivas@gmail.com>
2026-03-17 01:13:07 -0700

    añadi CNVenta

---
+++ CapaNegocio/CNVenta.cs
@@ -0,0 +1,29 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CNVenta
+    {
+        CDVenta dVenta = new CDVenta();
+
+        public int CrearPedido(int idUsuario, int idMesa)
+        {
+            return dVenta.InsertarPedido(idUsuario, idMesa);
+        }
+
+        public void AgregarDetalle(int idPedido, int idProducto, int cantidad, decimal precio)
+        {
+            dVenta.InsertarDetalle(idPedido, idProducto, cantidad, precio);
+        }
+
+        public void ConfirmarVenta(int idPedido)
+        {
+            dVenta.ConfirmarPedido(idPedido);
+        }
+    }
+}
